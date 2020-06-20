2c4a78f42aa2dba99aa08220eee9b85439c609ac
pad19 <pad19051999@gmail.com>
2020-06-20 14:19:26 +0300

    Add function isTriangle

---
+++ scripts/isTriangle/index.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @returns {boolean}
+ */
+export default function isTriangle(a, b, c) {
+    return isNaN(a) || isNaN(b) || isNaN(c) ? 'NaN' : a + b > c && a + c > b && b + c > a;
+}
