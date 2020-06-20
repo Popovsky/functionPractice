c711e8aeaa43e167c9bf201277fde53f7ca49f6d
pad19 <pad19051999@gmail.com>
2020-06-20 14:54:56 +0300

    Add function getTriangleArea

---
+++ scripts/getTriangleArea/index.js
@@ -0,0 +1,13 @@
+import isTriangle from "../isTriangle";
+
+/**
+ *
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @returns {number}
+ */
+export default function getTriangleArea(a, b, c) {
+    const p = (a + b + c) / 2;
+    return isTriangle(a, b, c) ? Math.sqrt(p * (p - a) * (p - b) * (p - c)) : 'Is not a triangle';
+}
