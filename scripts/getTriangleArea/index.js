5b32c70bf11dfd840a22a2d691f815d3b1692215
pad19 <pad19051999@gmail.com>
2020-06-21 14:46:00 +0300

    Fix function getTriangleArea

--- scripts/getTriangleArea/index.js
+++ scripts/getTriangleArea/index.js
@@ -5,7 +5,7 @@ import isTriangle from "../isTriangle";
  * @param {number} a
  * @param {number} b
  * @param {number} c
- * @returns {number}
+ * @returns {number|string}
  */
 export default function getTriangleArea(a, b, c) {
     const p = (a + b + c) / 2;
