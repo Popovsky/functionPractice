e763701b7a0bc293806fe1f42e11c0ed2a14e0a2
pad19 <pad19051999@gmail.com>
2020-06-21 15:05:43 +0300

    Add index.js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,15 @@
 import isAdult from "./isAdult";
+import checkMultiplicity from "./checkMultiplicity";
+import isTriangle from "./isTriangle";
+import getRectangleArea from "./getRectangleArea";
+import getTriangleArea from "./getTriangleArea";
+import getRhombusArea from "./getRhombusArea";
+import getCylinderArea from "./getCylinderArea";
 
 console.log('Is adult: ', isAdult(18));
+console.log('checkMultiplicity: ', checkMultiplicity(1, 2));
+console.log('Is triangle: ', isTriangle(3, 4, 5));
+console.log('Rectangle area: ', getRectangleArea(3, 4));
+console.log('Rectangle area: ', getTriangleArea(3, 4, 5));
+console.log('Rectangle area: ', getRhombusArea(3, 4));
+console.log('Rhombus area: ', getCylinderArea( 5, 4));
